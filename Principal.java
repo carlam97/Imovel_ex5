5c8ce4b310328e7a95c1bf99c6b9bb6167eb074c
carlam97 <carmartins04@hotmail.com>
2021-10-28 19:01:33 +0000

    Imovel

--- Principal.java
+++ Principal.java
@@ -8,6 +8,11 @@ public class Principal {
     novo.setEndereco("Vitor Euzebio da Silva");
     novo.setPreco(250000);
     novo.setAdicionalPreco(0.2*novo.getPreco());
-    System.out.println(novo.getPreco()+novo.getAdicionalPreco());
+    System.out.println("Preço imovel novo: R$"+(novo.getPreco()+novo.getAdicionalPreco()));
+
+    velho.setEndereco("Av Sampaio Vidal");
+    velho.setPreco(230000);
+    velho.setDescontoPreco(0.1*velho.getPreco());
+    System.out.println("Preço imovel velho: R$"+(velho.getPreco()- velho.getDescontoPreco()));
     }
 }
