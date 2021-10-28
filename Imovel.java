f0287e2bd96df845b4ca8731ad8233e4c154df10
carlam97 <carmartins04@hotmail.com>
2021-10-28 18:33:10 +0000

    Imovel

---
+++ Imovel.java
@@ -0,0 +1,22 @@
+public class Imovel{    // classe de modelagem
+
+    // atributos
+    private String endereco;
+    private double preco;
+
+    public void setEndereco(String endereco){
+        this.endereco=endereco;
+    }
+
+    public String getEndereco(){
+        return this.endereco;
+    }
+
+    public void setPreco(double preco){
+        this.preco=preco;
+    }
+
+    public double getPreco(){
+        return this.preco;
+    }
+}
---
+++ Novo.java
@@ -0,0 +1,13 @@
+public class Novo extends Imovel{
+
+    private double adicionalPreco;
+
+    // método de acesso e impressão
+    public void setAdicionalPreco(double adicionalPreco){
+        this.adicionalPreco=adicionalPreco;
+    }
+    public double getAdicionalPreco(){
+        return this.adicionalPreco;
+    }
+    
+}
